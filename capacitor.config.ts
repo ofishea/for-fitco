f3e9c7ace311390580347ff6c57b34bc330ecefb
ofishea <fishflowbean@gmail.com>
2023-06-22 10:11:41 +0100

    Installed Ionic and capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'for-fitco',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
