14d74f0604a3b112b58a86fdf4a34a56297407e4
ofishea <fishflowbean@gmail.com>
2023-06-22 11:15:02 +0100

    Done with Onboarding pages
    i finished the 3 onboarding pages and i've started setting up the backend environment

--- src/app/components/onboard3/onboard3.component.ts
+++ src/app/components/onboard3/onboard3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboard3',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Onboard3Component  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+    ) { }
 
   ngOnInit() {}
 
+  onResgister() {
+    this.router.navigate(['register']);
+  }
 }
